26735db6dcd4386bc7d7f984e12c721e44776f49
jaumeortola <jaumeortola@gmail.com>
2013-10-04 20:43:58 +0200

    èmfasi (també femení), actualitza script

--- build_dict.sh
+++ build_dict.sh
@@ -16,8 +16,8 @@ java -jar morfologik.jar fsa_dump -d catalan.dict -x >catalan_lt.txt
 # reorder columns for syntesis dictionary
 gawk -f synthesis.awk diccionari_tabs.txt >output2.txt
 # create synthesis dictionary with morfologik tools
-#java -jar morfologik.jar tab2morph -i output2.txt -o encoded.txt
-perl morph_data_ca.pl <output2.txt >encoded.txt
+java -jar morfologik.jar tab2morph -nw -i output2.txt -o encoded.txt
+#perl morph_data_ca.pl <output2.txt >encoded.txt
 export LC_ALL=C && sort encoded.txt | java -jar morfologik.jar fsa_build --sorted -f cfsa2 -o catalan_synth.dict
 # dump synthesis dictionary
 java -jar morfologik.jar fsa_dump -d catalan_synth.dict -x >catalan_synth_lt.txt
